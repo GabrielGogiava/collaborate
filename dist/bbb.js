fba2c670d1047aa0c71beea56262e432ddfb606f
gabrielgogiava <ForLaravel2020.1@gmail.com>
2024-01-17 22:56:49 +0400

    after GGSoft added line

---
+++ dist/bbb.js
@@ -0,0 +1,5 @@
+"use strict";
+const GGName = "George Gogiava";
+const GGAge = 53;
+console.log(`My name is ${GGName} and my age is ${GGAge}`);
+//# sourceMappingURL=bbb.js.map
---
+++ src/bbb.ts
@@ -0,0 +1,4 @@
+const GGName:string ="George Gogiava"
+const GGAge:number =53
+
+console.log(`My name is ${GGName} and my age is ${GGAge}`)
