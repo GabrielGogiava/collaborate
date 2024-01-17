598f04ec78f3755a67b526c20902c80c85009f08
GeorgeGogiava <ForLaravel2020.1@gmail.com>
2024-01-17 21:36:28 +0400

    first line of code

---
+++ dist/aaa.js
@@ -0,0 +1,5 @@
+"use strict";
+const gabo_name = "Gabriel gogiava";
+const gaboAge = 25;
+console.log(`My name is ${gabo_name}, and My age is ${gaboAge}`);
+//# sourceMappingURL=aaa.js.map
---
+++ src/aaa.ts
@@ -0,0 +1,6 @@
+const gabo_name:string="Gabriel gogiava"
+const gaboAge:number=25
+
+
+console.log(`My name is ${gabo_name}, and My age is ${gaboAge}`)
+
